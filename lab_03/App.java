88f33aeedf561fc6e87b65f11c891580b04ff84c
Timoshchenko Nikolay <silent_master@mail.ru>
2025-03-16 21:52:46 +0200

    lab_03

---
+++ lab_03/App.java
@@ -0,0 +1,9 @@
+package lab_03;
+
+import lab_03.src.Main;
+
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ lab_03/src/User.java
@@ -0,0 +1,19 @@
+package lab_03.src;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
